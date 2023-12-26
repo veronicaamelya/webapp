310a90374ba7bfef13ccf695129c2177d199020f
veronicaamelya <115519337+veronicaamelya@users.noreply.github.com>
2023-12-27 05:56:24 +0700

    Update app.sql

--- app.sql
+++ app.sql
@@ -1,26 +1,24 @@
-drop table if exists schedule;
-create table schedule (
+create table pemesanan (
 	id serial,
-	doctor_name text,
-	patient_name text,
+	play_name text,
+	customer_name text,
 	gender text,
-	symptom text,
+	game text,
 	handphone text,
-	address text,
 	waktu time,
 	tanggal date
 );
 
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
+insert into pemesanan (play_name, customer_name, gender, game, handphone, waktu, tanggal) 
 values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
+	('land', 'Veronica', 'female', '["bianglala"]', 62838, '08:00', '2023-10-01'),
+	('land', 'Jeno', 'male', '["roller coaster", "bombom car"]', 62838, '09:00', '2022-10-02'),
+	('air', 'Carissa', 'female', '["flying fox"]', 62838, '10:00', '2022-10-03'),
+	('watery', 'Winter', 'female', '["swimming"]', 62838, '11:00', '2022-10-04'),
+	('watery', 'Endita', 'male', '["jetski"]', 62838, '12:00', '2022-10-05'),
+	('land', 'Putri', 'female', '["roller coaster", "bianglala", "ice skating", "bowling"]', 62838, '08:00', '2022-10-06'),
+	('land', 'Marcella', 'male', '["bowling"]', 62838, '09:00', '2022-10-07'),
+	('air', 'Mingyu', 'male', '["paralayang", "ice skating"]', 62838, '10:00', '2022-10-08'),
+	('air', 'Anin', 'female', '["sky diving"]', 62838, '11:00', '2022-10-09'),
+	('watery', 'Yugi', 'male', '["snorkling"]', 62838, '12:00', '2022-10-11')
 ;
